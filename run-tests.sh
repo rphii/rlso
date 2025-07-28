03ea68891bede10ec679bdc56ce2e196dec67fad
rphii <raphael.seitz18@gmail.com>
2025-07-29 01:34:41 +0200

    tests

--- run-tests.sh
+++ run-tests.sh
@@ -10,7 +10,7 @@ trap cleanup EXIT
 dir=$(dirname $0)
 pushd $dir
 
-meson setup build -Db_coverage=true --reconfigure
+meson setup build -Db_coverage=true -Dtests=enabled --reconfigure
 meson compile -C build --clean
 meson test -C build #--wrapper 'valgrind --leak-check=full'
 ninja coverage -C build
