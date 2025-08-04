2ff9c860dd3df92955f6b679d0653f4a29ccb9a5
rphii <raphael.seitz18@gmail.com>
2025-08-04 23:10:27 +0200

    move things

--- src/so-as.c
+++ src/so-as.c
@@ -25,27 +25,6 @@ void so_as_cstr(So so, char *buf, size_t cap) {
 }
 
 ErrDecl _so_as_size(So ref, size_t *out, int base) {
-    if(!ref.len) return -1;
-    if(base > SO_AS_SIZE_BASE_MAX) return -1;
-    size_t result = 0;
-    for(size_t i = 0; i < ref.len; ++i) {
-        int val = static_so_as_unbase_char(ref.str[i]);
-        if(val < 0 || val >= base) {
-            return -1;
-        }
-        size_t next = result * base + val;
-        if(next < result) {
-            *out = SIZE_MAX;
-            return -1;
-        }
-        result = next;
-    }
-    *out = result;
-    return 0;
-}
-
-ErrDecl so_as_size(So so, size_t *out, int base) {
-    So ref = so;
     size_t base_use = base ? base : 10;
     if(ref.len >= 2 && ref.str[0] == '0') {
         so_shift(&ref, 1);
@@ -59,7 +38,28 @@ ErrDecl so_as_size(So so, size_t *out, int base) {
             base_use = base ? base : 8;
         }
     }
-    int result = _so_as_size(ref, out, base_use);
+    if(!ref.len) return -1;
+    if(base_use > SO_AS_SIZE_BASE_MAX) return -1;
+    size_t result = 0;
+    for(size_t i = 0; i < ref.len; ++i) {
+        int val = static_so_as_unbase_char(ref.str[i]);
+        if(val < 0 || val >= base_use) {
+            return -1;
+        }
+        size_t next = result * base_use + val;
+        if(next < result) {
+            *out = SIZE_MAX;
+            return -1;
+        }
+        result = next;
+    }
+    *out = result;
+    return 0;
+}
+
+ErrDecl so_as_size(So so, size_t *out, int base) {
+    So ref = so;
+    int result = _so_as_size(ref, out, base);
     return result;
 }
 
