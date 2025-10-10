60ec9baac921f98aa23b30de4f32547f3693d511
rphii <raphael.seitz18@gmail.com>
2025-10-10 17:54:48 +0200

    don't want that printff

--- src/rlso/so-cmp.c
+++ src/rlso/so-cmp.c
@@ -56,7 +56,6 @@ int so_cmp_cs(So a, So b) {
 /* compare {{{ */
 
 int so_cmp_p(So *a, So *b) {
-    printff("ptr %p <> %p",a,b);
     if(!a && !b) return 0;
     if(!a) return so_len(*b);
     if(!b) return so_len(*a);
