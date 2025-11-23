8235e80c7fc80e8eefa81eb065ba1628931c7377
rphii <raphael.seitz18@gmail.com>
2025-11-23 21:26:02 +0000

    fix hash maybe

--- src/rlso/so-hash.c
+++ src/rlso/so-hash.c
@@ -9,7 +9,7 @@ inline size_t so_hash(const So str) { /*{{{*/
     char *e = so_itE(str);
     size_t hash = 0xcbf29ce484222325;
     while(c < e) {
-        hash ^= (*c)++;
+        hash ^= *(c++);
         hash *= 0x00000100000001b3;
     }
     return hash;
