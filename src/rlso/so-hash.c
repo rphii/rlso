567a5ebf73a8b49a7e7003368b9d418d7c7e75af
rphii <raphael.seitz18@gmail.com>
2025-11-23 20:20:55 +0000

    fix typo and actually use FNV-1a instead of FNV-1

--- src/rlso/so-hash.c
+++ src/rlso/so-hash.c
@@ -3,14 +3,14 @@
 #include <rlc/platform-detect.h>
 
 size_t so_hash(const So str) { /*{{{*/
-    /* FNV1-a */
+    /* FNV-1a */
     /* TODO check if we are actually on 64 bit */
-    size_t i = 0;
+    char *c = so_it0(str);
+    char *e = so_itE(str);
     size_t hash = 0xcbf29ce484222325;
-    while(i < so_len(str)) {
-        unsigned char c = str.str[i++];
+    while(c < e) {
+        hash ^= (*c)++;
         hash *= 0x00000100000001b3;
-        hash ^= c;
     }
     return hash;
 } /*}}}*/
